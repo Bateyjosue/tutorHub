7f2bb8cb2eb569b51fbc7ef98b489e5362f65e54
Kene <kayyungx@gmail.com>
2023-03-02 14:23:15 +0100

    add lint error solution

--- spec/views/tutors/index.html.erb_spec.rb
+++ spec/views/tutors/index.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "tutors/index.html.erb", type: :view do
+RSpec.describe 'tutors/index.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
