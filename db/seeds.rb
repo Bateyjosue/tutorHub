1f5e005f60a67c3c1059c23a253f29a02a9ea80b
Kene <kayyungx@gmail.com>
2023-03-06 11:35:09 +0100

    seed database with tutor information

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# create a user
+user = User.create(email: 'example@example.com', password: 'password')
+
+# create some tutors for the user
+tutors = [
+  {
+    name: 'John Doe',
+    image: 'https://example.com/john-doe.jpg',
+    description: 'Experienced tutor in math and science',
+    price: 50.0,
+    city: 'San Francisco',
+    user: user
+  },
+  {
+    name: 'Jane Smith',
+    image: 'https://example.com/jane-smith.jpg',
+    description: 'English and history tutor with a passion for teaching',
+    price: 40.0,
+    city: 'New York',
+    user: user
+  },
+  {
+    name: 'Bob Johnson',
+    image: 'https://example.com/bob-johnson.jpg',
+    description: 'Expert in computer programming and web development',
+    price: 60.0,
+    city: 'Los Angeles',
+    user: user
+  }
+]
+
+# create each tutor record in the database
+tutors.each do |tutor_params|
+  Tutor.create(tutor_params)
+end
