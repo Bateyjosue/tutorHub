d1c8fecf851d379014faeca0fad1d7b3450db04c
Bateyjosue <josuebatey19@gmail.com>
2023-03-06 11:12:10 +0200

    config files

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_210957) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_085930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "date"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "tutor_id"
+  end
+
+  create_table "tutors", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "description"
+    t.decimal "price"
+    t.string "city"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tutors_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_210957) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tutors", "users"
 end
