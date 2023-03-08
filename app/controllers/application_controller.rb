6a275d3326333ec4b3b5c3f56634a74550ca65cb
Kene <kayyungx@gmail.com>
2023-03-08 20:35:51 +0100

    verify user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    skip_before_action :verify_authenticity_token
     def csrf_token
         render json: { csrf_token: form_authenticity_token }
     end  
--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,5 +1,4 @@
 class TutorsController < ApplicationController
-  skip_before_action :verify_authenticity_token
 
   def index
     @tutors = Tutor.all
