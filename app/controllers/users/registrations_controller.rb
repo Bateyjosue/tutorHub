c83b613a621108be21e2453acc98f0980ea44ebe
Kene <kayyungx@gmail.com>
2023-03-08 20:46:57 +0100

    Correct lint error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,11 +4,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     user = User.new(user_params)
     if user.save
-      render json: status: { message: 'User created successfully' }, status: :created
+      render json: { message: 'User created successfully' }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
-end
+  end
 
   private
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+    describe 'POST #create' do
+        context 'when valid user parameters are provided' do
+          let(:user_params) { attributes_for(:user) }
+    
+          it 'creates a new user' do
+            expect do
+              post :create, params: { user: user_params }
+            end.to change(User, :count).by(1)
+          end
+    
+          it 'returns a successful response' do
+            post :create, params: { user: user_params }
+            expect(response).to have_http_status(:created)
+          end
+    
+          it 'returns a success message' do
+            post :create, params: { user: user_params }
+            expect(JSON.parse(response.body)['message']).to eq('User created successfully')
+          end
+        end
+    
+        context 'when invalid user parameters are provided' do
+          let(:user_params) { attributes_for(:user, name: nil) }
+    
+          it 'does not create a new user' do
+            expect do
+              post :create, params: { user: user_params }
+            end.not_to change(User, :count)
+          end
+    
+          it 'returns an unprocessable entity response' do
+            post :create, params: { user: user_params }
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+    
+          it 'returns an error message' do
+            post :create, params: { user: user_params }
+            expect(JSON.parse(response.body)['errors']).to include("Name can't be blank")
+          end
+        end
+      end
 end
